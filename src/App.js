8c58c72f1268508a17e33cb2328e4e82fb1ea916
verona-hub <68257343+batman-project@users.noreply.github.com>
2021-02-21 19:53:19 +0100

    Changes: - added css rules - added Switch and Route to Homepage component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+// Components
+import About from './components/About';
+import Homepage from './components/Homepage';
+import Navbar from './components/Navbar';
+
 
 
 function App() {
   return (
-    <div className="App">
-      My App
+    <div>
+      <Navbar />
+      <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/about" component={About} />
+      </Switch>
     </div>
   );
 }
