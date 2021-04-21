38b05fa5fed70ceebf2dbf2fbe42af5cd0fed722
Zoran G <68257343+verona-hub@users.noreply.github.com>
2021-04-21 18:17:29 +0200

    Changes: - added hover opacity to arrows and to slider buttons - added transition to bullet hover - started the AirQualityData.js component, moved data from AirQuality.js

--- src/components/Page/Info/AirQuality.js
+++ src/components/Page/Info/AirQuality.js
@@ -1,65 +1,10 @@
 import React, { useState } from 'react';
-
+import { data } from '../../Utility/AirQualityData';
 
 const AirQuality = ({ airQuality }) => {
 
     const [ indexInfoVisible, setIndexInfoVisible] = useState(false);
 
-    const data = {
-        case1: {
-            color: '#9cff9c',
-            index: 'Index 1',
-            level: 'Low'
-        },
-        case2: {
-            color: '#66cb55',
-            index: 'Index 2',
-            level: 'Low'
-        },
-        case3: {
-            color: '#5a9f4f',
-            index: 'Index 3',
-            level: 'Low'
-        },
-        case4: {
-            color: '#fbfb66',
-            index: 'Index 4',
-            level: 'Moderate'
-        },
-        case5: {
-            color: '#ffcf00',
-            index: 'Index 5',
-            level: 'Moderate'
-        },
-        case6: {
-            color: '#ff9a00',
-            index: 'Index 6',
-            level: 'Moderate'
-        },
-        case7: {
-            color: '#ff6464',
-            index: 'Index 7',
-            level: 'High'
-        },
-        case8: {
-            color: '#d85454',
-            index: 'Index 8',
-            level: 'High'
-        },
-        case9: {
-            color: '#e63d3d',
-            index: 'Index 9',
-            level: 'High'
-        },
-        case10: {
-            color: '#F00',
-            index: 'Index 10',
-            level: 'Very High'
-        },
-        mix: {
-            white: '#fff'
-        }
-    }
 
     const { case1, case2, case3, case4, case5, case6, case7, case8, case9, case10, mix} = data;
     let { co, no2, o3, so2, pm2_5, pm10 } = airQuality;
