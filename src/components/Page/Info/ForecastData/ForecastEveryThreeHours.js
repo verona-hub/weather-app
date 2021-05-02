6177353a1f5ced91ceca603d0a4d98eb95217fef
Zoran G <68257343+verona-hub@users.noreply.github.com>
2021-05-02 17:15:45 +0100

    Changes: - reduced Forecast slider height and adapted Hourly and Daily forecast

--- src/components/Page/Info/ForecastData/ForecastEveryThreeHours.js
+++ src/components/Page/Info/ForecastData/ForecastEveryThreeHours.js
@@ -9,15 +9,17 @@ const ForecastEveryThreeHours = ({ hour }) => {
         const { condition: { text, icon }, time, temp_c } = item;
 
         return index % 3 === 0 && (
-            <div key={ index } className="item_wrapper">
-                <div className="item">{ timeForecastHourly(time) } </div>
-                <div className="item">
-                    <img src={ icon } alt="weather conditions icon"/>
-                    <div className="text_wrapper">
-                        <span> { text } </span>
+            <div key={ index } className="ForecastEveryThreeHours">
+                <div className="item_wrapper">
+                    <div className="item">{ timeForecastHourly(time) } </div>
+                    <div className="item">
+                        <img src={ icon } alt="weather conditions icon"/>
+                        <div className="text_wrapper">
+                            <span> { text } </span>
+                        </div>
                     </div>
+                    <div className="item"> { temp_c }&#8451; </div>
                 </div>
-                <div className="item"> { temp_c }&#8451; </div>
             </div>
         )
     });
