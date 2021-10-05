b880436310856ac074f59a57e91f8202d05a0341
Zoran G <68257343+verona-hub@users.noreply.github.com>
2021-10-05 19:48:05 +0100

    Changes: - darkmode welcome frame on About page - removed navbar_other

--- src/components/Page/Navbar.js
+++ src/components/Page/Navbar.js
@@ -12,26 +12,17 @@ const Navbar = ({ text, emptyContent, spinner, errorMessage, errorCode, clearErr
 
     const headerHomePage = window.location.pathname === '/';
     const aboutPage = window.location.pathname === '/about';
-    const otherPages = window.location.pathname !== '/' && window.location.pathname !== '/about';
 
     const headerChange = aboutPage ? 'header_100'
         : ( headerHomePage && emptyContent  ? ' header_100'
             : (window.location.pathname !== ('/' || '/about') ? ' header_404' : 'header_70'));
 
-
-    // const homepageOrAboutpage = (headerHomePage && emptyContent) && window.location.pathname === '/about';
-    // const headerChange = homepageOrAboutpage ? 'header_100' : 'header_404';
-
-    const otherNavbar = otherPages ? 'navbar_other' : 'navbar';
-    const navbar = modal ? 'navbar navbar_dark' : `navbar ${otherNavbar}`;
-
     const headerDark = darkMode && !modal && 'header_dark_mode';
     const header = `header ${headerChange} ${headerDark}`;
 
-
     return (
         <div className={ header }>
-            <nav className= { navbar }>
+            <nav className= 'navbar'>
                 <NavLink to='/' activeClassName='active' exact className='logo_wrapper'>
                     <img src={ logo } alt="logo icon" className="logo_img"/>
                     <div className='nav_link_item'>Weather App</div>
